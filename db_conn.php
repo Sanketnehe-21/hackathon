574b8d8985a9d944672f23cddbcc714d9eab19f8
Sanketnehe-21 <sanketnehe21@gmail.com>
2024-04-11 19:01:57 +0530

    added

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,10 @@
 <?php  
 
-$sname = "phpweb.cefkjzu19slj.us-east-1.rds.amazonaws.com";
+$sname = "php-website.cefkjzu19slj.us-east-1.rds.amazonaws.com";
 $uname = "admin";
 $password = "12345678";
 
-$db_name = "education";
+$db_name = "login2";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
