d2ebfd389fc447beac8c120b94f7fe8d5b73a839
Sanketnehe-21 <sanketnehe21@gmail.com>
2024-04-11 19:18:15 +0530

    config updated

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "login2");
+$conn = mysqli_connect("php-website.cefkjzu19slj.us-east-1.rds.amazonaws.com", "admin", "12345678", "login2");
 
 if (!$conn) {
     echo "Connection Failed";
